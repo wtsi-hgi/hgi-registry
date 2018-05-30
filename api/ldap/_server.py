186b583039ee900be9fa715095735f7161996f0d
Christopher Harrison <ch12@sanger.ac.uk>
2018-05-30 14:55:22 +0100

    First stab at async LDAP search

--- api/ldap/_server.py
+++ api/ldap/_server.py
@@ -24,9 +24,11 @@ from common import types as T
 from ._scope import Scope
 
 
+_EntryT = T.Dict[str, T.Union[T.Text, T.ByteString]]
+
 class LDAPServer(LDAPObject, ResultProcessor):
     """ LDAP connection object with asynchronous searching """
-    async def search(self, base:str, scope:Scope, search:str = "(objectClass=*)", attrs:T.Optional[T.List[str]] = None):
+    async def search(self, base:str, scope:Scope, search:str = "(objectClass=*)", attrs:T.Optional[T.List[str]] = None) -> T.AsyncIterator[T.Tuple[str, _EntryT]]:
         """
         Invoke an LDAP search and return results asynchronously
 
@@ -34,5 +36,10 @@ class LDAPServer(LDAPObject, ResultProcessor):
         @param   scope   Search scope
         @param   search  Search term
         @param   attrs   List of attributes; None for everything (default)
+        @return  Asynchronous generator of DN/entry tuples
         """
-        raise NotImplementedError
+        msgid = super().search(base, scope.value, search, attrs)
+
+        async for _type, data, _msgid, _controls in self.allresults(msgid):
+            # data is a tuple of DN (string) and payload (dictionary)
+            yield data
