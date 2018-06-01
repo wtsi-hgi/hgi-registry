3705ec7570f9a431c42daf1570383f37035a8414
Christopher Harrison <ch12@sanger.ac.uk>
2018-06-01 09:26:19 +0100

    Remove LDAPServer test
    This is effectively an integration test. What we were trying to unit
    test, with complicated patching, was that its trivial control flow does
    what we think it does. This is unnecessarily verbose and tied to the
    implementation.

--- tests/api/test_ldap.py
+++ tests/api/test_ldap.py
@@ -18,13 +18,18 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
 import unittest
-from unittest.mock import patch
 
 from tests import async_test
 import api.ldap._server as ldap
 
 
 def _mock_results():
+    """
+    This generator yields the same structure as that returned by
+    ldap.resiter's results generator. It's not clear from the
+    documentation why the data tuple is wrapped up in a list, but it's
+    not our place to ask...
+    """
     for _ in range(10):
         yield "foo", [("dn", "entry")], "bar", "quux"
 
@@ -35,16 +40,3 @@ class TestResultGenerator(unittest.TestCase):
         async for dn, entry in ldap._SearchResults(_mock_results()):
             self.assertEqual(dn, "dn")
             self.assertEqual(entry, "entry")
-
-
-class TestLDAPServer(unittest.TestCase):
-    @async_test
-    @patch("api.ldap._server.ResultProcessor", spec=True)
-    @patch("api.ldap._server.LDAPObject", spec=True)
-    async def test_search(self, mock_ldap, mock_results):
-        # FIXME Mocking methods from superclasses
-        ldap.LDAPServer.__bases__ = (mock_ldap.__class__, mock_results.__class__)
-        s = ldap.LDAPServer("ldap://example.com")
-        s.search("dc=example,dc=com", ldap.Scope.Base, "(foo=bar)")
-
-        # TODO Can this even be done?...
