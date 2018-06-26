a1c20a089e22d8d4b5b5d5d4704a6e17bd10476a
Christopher Harrison <ch12@sanger.ac.uk>
2018-06-26 13:08:27 +0100

    WIP: Internal Server error middleware

---
+++ api/httpd/_middleware.py
@@ -0,0 +1,33 @@
+"""
+Copyright (c) 2018 Genome Research Ltd.
+
+Author: Christopher Harrison <ch12@sanger.ac.uk>
+
+This program is free software: you can redistribute it and/or modify it
+under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or (at
+your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from common.logging import Level, log
+from ._types import Application, Request, Response, Handler
+
+
+async def catch500(app:Application, handler:Handler) -> Handler:
+    """ Internal Server Error catch-all """
+    async def _middleware(request:Request) -> Response:
+        try:
+            return await handler(request)
+
+        except Exception:
+            pass
+
+    return _middleware
